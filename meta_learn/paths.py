898ffe87fc45be124ce8c2e7a90f9b6d5fb2d24b
Simon Blanke <simon.blanke@yahoo.com>
2024-02-18 11:23:28 +0100

    add base-path

--- meta_learn/paths.py
+++ meta_learn/paths.py
@@ -6,18 +6,20 @@ import os
 
 
 class Paths:
+    base_path_name = "data_meta_learn"
+
     def __init__(self, path):
-        self.path = path
+        self.base_path_name = os.path.join(path, self.base_path_name)
 
     def get_model_dir(self, model_id):
-        return os.path.join(self.path, model_id)
+        return os.path.join(self.base_path_name, model_id)
 
     def get_dataset_dir(self, model_id, dataset_id):
-        return os.path.join(self.path, model_id, dataset_id)
+        return os.path.join(self.base_path_name, model_id, dataset_id)
 
     def create_meta_learn_dir(self):
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
+        if not os.path.exists(self.base_path_name):
+            os.makedirs(self.base_path_name)
 
     def create_model_dir(self, model_id):
         path = self.get_model_dir(model_id)
