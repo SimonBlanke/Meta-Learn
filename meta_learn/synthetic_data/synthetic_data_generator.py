74a66b8af7c00ea6e5a0f5b32525fbdbb4b907e7
Simon Blanke <simon.blanke@yahoo.com>
2024-02-18 11:23:48 +0100

    add parameters to collect-method

--- meta_learn/synthetic_data/synthetic_data_generator.py
+++ meta_learn/synthetic_data/synthetic_data_generator.py
@@ -8,32 +8,25 @@ from hyperactive import Hyperactive
 
 from .synthetic_data_parameters import dataset_dict
 from ..tabular.regression.meta_learn import MetaLearn
-from .objective_functions import dtc_function
 
 
 class SyntheticDataGenerator:
-    def __init__(self) -> None:
-        self.meta_learn = MetaLearn("./data_meta_learn")
+    def __init__(self, base_path=".") -> None:
+        self.meta_learn = MetaLearn(base_path)
         self.dataset_dict = dataset_dict
 
     def generate(self, dataset_para):
         return make_classification(**dataset_para)
 
-    def collect(self, model_id, n_iter, n_jobs=1):
+    def collect(self, objective_function, search_space, model_id, n_iter, n_jobs=1):
         for dataset_id in self.dataset_dict.keys():
             dataset_para = dataset_dict[dataset_id]
 
             X, y = self.generate(dataset_para)
 
-            dtc_function_dec = self.meta_learn.collect(X, y, model_id, dataset_id)(
-                dtc_function
-            )
-
-            search_space = {
-                "max_depth": list(range(2, 100)),
-                "min_samples_split": list(range(2, 100)),
-                "min_samples_leaf": list(range(1, 100)),
-            }
+            objective_function_dec = self.meta_learn.collect(
+                X, y, model_id, dataset_id
+            )(objective_function)
 
             pass_through = {
                 "X": X,
@@ -42,7 +35,7 @@ class SyntheticDataGenerator:
 
             hyper = Hyperactive(verbosity=["progress_bar"])
             hyper.add_search(
-                dtc_function_dec,
+                objective_function_dec,
                 search_space,
                 n_iter=n_iter,
                 n_jobs=n_jobs,
