f2180727c3522113a8094a503ef6a73f995539a0
Simon Blanke <simon.blanke@yahoo.com>
2024-02-19 17:25:59 +0100

    rename + add default-reference-models

---
+++ meta_learn/tabular/classification/default_reference_models.py
@@ -0,0 +1,33 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+
+from sklearn.model_selection import cross_val_score
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
+
+def reg_ref_model(X, y):
+    reg = LogisticRegression()
+    reg.fit(X, y)
+    return reg.score(X, y)
+
+
+def dtr_ref_model(X, y):
+    dtr = DecisionTreeClassifier()
+    return cross_val_score(dtr, X, y, cv=5).mean()
+
+
+def gbr_ref_model(X, y):
+    gbr = GradientBoostingClassifier()
+    return cross_val_score(gbr, X, y, cv=5).mean()
+
+
+ref_models = {
+    "reg": reg_ref_model,
+    "dtr": dtr_ref_model,
+    "gbr": gbr_ref_model,
+}
