a97c06ba9f2f72fe54f61a8a440229b71ae226d0
Simon Blanke <simon.blanke@yahoo.com>
2024-02-19 17:24:06 +0100

    adapt code to new feature-generator

--- meta_learn/tabular/regression/default_reference_datasets.py
+++ meta_learn/tabular/regression/default_reference_datasets.py
@@ -31,20 +31,3 @@ ref_models = {
     "dtr": dtr_ref_model,
     "gbr": gbr_ref_model,
 }
-
-
-def dataset_feature_generator(X, y):
-    ref_scores = {}
-
-    print("Generate dataset features...", end="\r")
-    for model_name in ref_models.keys():
-        ref_scores[model_name] = ref_models[model_name](X, y)
-
-    ref_scores["n_samples"] = X.shape[0]
-    ref_scores["n_features"] = X.shape[1]
-
-    print("Dataset features generated    ")
-
-    # print("\n ref_scores \n", ref_scores, "\n")
-
-    return ref_scores
--- meta_learn/tabular/regression/meta_learn.py
+++ meta_learn/tabular/regression/meta_learn.py
@@ -2,12 +2,13 @@
 # Email: simon.blanke@yahoo.com
 # License: MIT License
 
+
+from .default_reference_datasets import ref_models
+from ..dataset_feature_generator import DatasetFeatureGenerator
 from ...meta_learn_core import MetaLearnCore
-from .default_reference_datasets import dataset_feature_generator
 
 
 class MetaLearn(MetaLearnCore):
     def __init__(self, path):
         super().__init__(path)
-
-        self.dataset_feature_generator = dataset_feature_generator
+        self.dataset_feature_generator = DatasetFeatureGenerator(ref_models)
