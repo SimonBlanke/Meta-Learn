edde98eb60a1e72d6ceaf33b9a54ec8a06f85214
Simon Blanke <simon.blanke@yahoo.com>
2024-03-04 17:18:21 +0100

    add remove method to meta-regressor

--- meta_learn/base_meta_regressor.py
+++ meta_learn/base_meta_regressor.py
@@ -2,9 +2,41 @@
 # Email: simon.blanke@yahoo.com
 # License: MIT License
 
-import os
+import os, sys
 from joblib import load, dump
 from sklearn.ensemble import GradientBoostingRegressor
+
+
+def query_yes_no(question, default="no"):
+    # ref: https://stackoverflow.com/questions/3041986/apt-command-line-interface-like-yes-no-input
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
 
 
 class BaseMetaRegressor:
@@ -35,6 +67,18 @@ class BaseMetaRegressor:
     def load(self, objective_function):
         path = self.generate_path(objective_function.__name__)
         self.m_reg = load(path)
+
+    def remove_confirmed(self, objective_function):
+        path = self.generate_path(objective_function.__name__)
+        os.remove(path)
+
+    def remove(self, objective_function, always_confirm=False):
+        if always_confirm:
+            self.remove_confirmed(objective_function)
+        else:
+            question = "Remove pretrained meta regressor?"
+            if query_yes_no(question):
+                self.remove_confirmed(objective_function)
 
     def fit(self, X_meta, y_meta, drop_duplicates=True):
         if drop_duplicates:
