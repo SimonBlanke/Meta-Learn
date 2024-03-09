5c5befc27b6895f6adb1d9910fc30aa4816a967c
Simon Blanke <simon.blanke@yahoo.com>
2024-03-09 14:36:24 +0100

    use search-data/dataset-features classes in base-meta-data class

--- meta_learn/base_meta_data.py
+++ meta_learn/base_meta_data.py
@@ -13,6 +13,7 @@ import pandas as pd
 from .paths import Paths
 from .search_data import SearchData
 from .dataset_features import DatasetFeatures
+from .paths import SyntheticMetaDataPaths
 
 
 class BaseMetaData:
@@ -32,11 +33,11 @@ class BaseMetaData:
             raise ValueError
 
     def collect(self, X, y, model_id, dataset_id):
-        self.search_data = SearchData(model_id, dataset_id)
-        self.dataset_features = DatasetFeatures(model_id, dataset_id)
+        search_data = SearchData(model_id, dataset_id)
+        dataset_features = DatasetFeatures(model_id, dataset_id)
 
         ref_scores = self.dataset_feature_generator.create(X, y)
-        self.dataset_features.dump(ref_scores)
+        dataset_features.dump(ref_scores)
 
         def decorator(model):
             def wrapper(access):
@@ -50,7 +51,7 @@ class BaseMetaData:
                 else:
                     parameter["score"] = result
 
-                self.search_data.append(parameter)
+                search_data.append(parameter)
 
                 return result
 
@@ -59,36 +60,27 @@ class BaseMetaData:
         return decorator
 
     def get_meta_data_X(self, search_space, X, y):
-        print("\n approx_best_para")
-
         keys, values = zip(*search_space.items())
         permutations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
 
         all_parameters = pd.DataFrame(permutations_dicts)
 
-        ref_scores = self.dataset_feature_generator.create(X, y)
-        meta_data_test = all_parameters.assign(**ref_scores)
+        dataset_features = self.dataset_feature_generator.create(X, y)
+        meta_data_test = all_parameters.assign(**dataset_features)
 
         print("create", len(all_parameters), "samples")
         return meta_data_test
 
     def get_meta_data(self, model_id: str) -> Tuple[pd.core.frame.DataFrame]:
-        model_dir = self.path.get_model_dir(model_id)
-        dataset_id_l = [name for name in os.listdir(model_dir)]
-
         meta_data_train_l = []
-        for dataset_id in dataset_id_l:
-            dataset_path = os.path.join(model_dir, dataset_id)
+        for dataset_id in SyntheticMetaDataPaths.dataset_ids(model_id):
+            search_data_c = SearchData(model_id, dataset_id)
+            dataset_features_c = DatasetFeatures(model_id, dataset_id)
 
-            dataset_features_path = os.path.join(dataset_path, "dataset_features.json")
-            search_data_path = os.path.join(dataset_path, "search_data.csv")
+            dataset_features = dataset_features_c.load()
+            search_data = search_data_c.load()
 
-            with open(dataset_features_path, "r") as f:
-                ref_scores = json.load(f)
-
-            search_data = pd.read_csv(search_data_path)
-
-            meta_data_train = search_data.assign(**ref_scores)
+            meta_data_train = search_data.assign(**dataset_features)
             meta_data_train_l.append(meta_data_train)
 
         meta_data_train = pd.concat(meta_data_train_l, axis=0, ignore_index=True)
