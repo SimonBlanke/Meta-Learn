e02c775950dc714ae768ab53054daef1bace7f65
Simon Blanke <simon.blanke@yahoo.com>
2024-03-09 13:42:05 +0100

    small update; add path-methods

--- meta_learn/paths/meta_regressor_paths.py
+++ meta_learn/paths/meta_regressor_paths.py
@@ -4,7 +4,6 @@
 
 import os
 
-
 from .paths import Paths
 
 
--- meta_learn/paths/synthetic_meta_data_paths.py
+++ meta_learn/paths/synthetic_meta_data_paths.py
@@ -4,7 +4,6 @@
 
 import os
 
-
 from .paths import Paths
 
 
@@ -12,4 +11,21 @@ class SyntheticMetaDataPaths(Paths):
     search_data: str = "search_data.csv"
     dataset_features: str = "dataset_features.json"
 
-    synthetic_meta_data_path: str = os.path.join(Paths.pkg_data, "synthetic_meta_data")
+    synthetic_meta_data_base_path: str = os.path.join(
+        Paths.pkg_data, "synthetic_meta_data"
+    )
+
+    def model_dataset(cls, model_id, dataset_id):
+        return os.path.join(cls.synthetic_meta_data_base_path, model_id, dataset_id)
+
+    @Paths.create_dir
+    @classmethod
+    def dataset_features(cls, model_id, dataset_id):
+        return os.path.join(
+            cls.model_dataset(model_id, dataset_id), "dataset_features.json"
+        )
+
+    @Paths.create_dir
+    @classmethod
+    def search_data(cls, model_id, dataset_id):
+        return os.path.join(cls.model_dataset(model_id, dataset_id), "search_data.json")
