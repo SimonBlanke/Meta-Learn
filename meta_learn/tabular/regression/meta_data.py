2de7c2aacd99082804ef1320dbaa809e60423fa3
Simon Blanke <simon.blanke@yahoo.com>
2024-03-09 17:42:12 +0100

    add base class

--- meta_learn/tabular/regression/meta_data.py
+++ meta_learn/tabular/regression/meta_data.py
@@ -6,9 +6,10 @@
 from .default_reference_models import ref_models
 from ..dataset_feature_generator import DatasetFeatureGenerator
 from ...base_meta_data import BaseMetaData
+from .model_and_dataset_type import ModelAndDatasetType
 
 
-class MetaData(BaseMetaData):
-    def __init__(self, path):
-        super().__init__(path)
+class MetaData(BaseMetaData, ModelAndDatasetType):
+    def __init__(self, base_path):
+        super().__init__(base_path)
         self.dataset_feature_generator = DatasetFeatureGenerator(ref_models)
