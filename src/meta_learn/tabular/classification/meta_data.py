7709b8232ae49044fde3b311110780c535621d59
Simon Blanke <simon.blanke@yahoo.com>
2025-07-22 15:36:12 +0200

    add docstrings to meta_data.py

--- src/meta_learn/tabular/classification/meta_data.py
+++ src/meta_learn/tabular/classification/meta_data.py
@@ -10,6 +10,16 @@ from .model_and_dataset_type import ModelAndDatasetType
 
 
 class MetaData(BaseMetaData, ModelAndDatasetType):
+    """Meta-data manager for tabular classification tasks.
+
+    Specializes BaseMetaData for tabular classification problems,
+    configuring appropriate dataset feature extraction using
+    classification-specific reference models.
+
+    Attributes:
+        dataset_feature_generator: Configured feature generator instance
+    """
+
     def __init__(self, base_path):
         super().__init__(base_path)
         self.dataset_feature_generator = DatasetFeatureGenerator(ref_models)
